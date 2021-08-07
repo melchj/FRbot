699946a2862860300c38c221d7fe10db35999fe0
Jack Melcher <jack.melcher@gmail.com>
2021-08-07 11:05:42 -0400

    basic tingo list implemented

--- cogs/Tingo.py
+++ cogs/Tingo.py
@@ -1,3 +1,4 @@
+from sqlite3.dbapi2 import Cursor
 import discord
 from discord.ext import commands
 import asyncio
@@ -6,6 +7,7 @@ from datetime import datetime
 
 from discord.ext.commands.errors import ExtensionNotLoaded
 
+# TODO: ensure the help messages are useful and pop up at the right time(s)
 class Tingo(commands.Cog):
     '''Tingo'''
 
@@ -53,6 +55,7 @@ class Tingo(commands.Cog):
         for victim in args:
             victim = str(victim).title()
             # put entry in database for victim with image url
+            # TODO: do i actually need the channel id? It can be server wide instead of channel specific, i think
             cursor.execute(f"INSERT INTO tingodex (guild_id,channel_id,trainer_id,victim_name,image_path) VALUES({ctx.guild.id},{ctx.channel.id},{ctx.message.author.id},'{victim}','{imgPath}')")
         
         db.commit()
@@ -62,9 +65,32 @@ class Tingo(commands.Cog):
     @tingo.command()
     async def list(self, ctx, *args):
         # ".tingo list" -- send a list of all the different people (names) this person has captured
-        # show the tingo pictures? only a certain number of pictures?
-        # have a ".tingo list <name>" command to show all the pictures for that person?
-        return
+        db = sqlite3.connect('tingo.sqlite')
+        cursor = db.cursor()
+
+        # list all UNIQUE victims of this trainer on this discord server
+        cursor.execute(f"SELECT DISTINCT victim_name from tingodex WHERE guild_id={ctx.guild.id} AND trainer_id={ctx.message.author.id}")
+        result = cursor.fetchall()
+        print(result)
+
+        if not result:
+            await ctx.send('No results! You haven\'t captured anything?')
+        else:
+            embed = discord.Embed(color=0xf5f2ca) # TODO: make a tingo-esque color
+            victims = ''
+            for value in result:
+                victims = victims + str(value[0]) + '\n'
+            embed.add_field(name=f'{ctx.message.author.display_name}\'s Tingo Victims', value=victims, inline=True)
+            embed.set_author(name='Free Ring Tingos', icon_url=f'{ctx.guild.icon_url}')
+            embed.set_footer(text='Do \".tingo show <victim>\" for more info!')
+            embed.set_thumbnail(url=f'{ctx.guild.icon_url}') # TODO: make this a tingo icon?
+
+            await ctx.send(embed=embed)
+        # TODO: show the tingo pictures? only a certain number of pictures?
+        # TODO: have a ".tingo list <name>" command to show all the pictures for that person?
+        # or are the above just for 
+
+    # TODO: add a ".tingo backup" command, just like .perc backup? tho what to do with images...?
 
 def setup(bot):
     bot.add_cog(Tingo(bot))
